66aef1a7bcfca38ea29c35f76ad594dd175d8f13
TravaValera <i.burk@mail.ru>
2024-04-16 14:07:56 +0300

    Автоключ работает

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-# Определение алфавита и соответствующих чисел
 alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 numbers = list(range(33))
 
-# Создание словаря для преобразования каждой буквы в соответствующее число
+
 letter_to_number = dict(zip(alphabet, numbers))
 
-# Создание словаря для преобразования каждого числа в соответствующую букву
+
 number_to_letter = dict(zip(numbers, alphabet))
 
 
 def encrypt(word, key):
-    # Преобразование слова и ключа в числа
+
     word_numbers = [letter_to_number[letter] for letter in word]
     key_numbers = [letter_to_number[letter] for letter in key]
 
-    # Расширение ключа до длины слова, если ключ короче
+
     extended_key_numbers = key_numbers + word_numbers
     extended_key_numbers = extended_key_numbers[:len(word)]
 
-    # Шифрование слова
+
     encrypted_numbers = [(word_number + key_number) % 33 for word_number, key_number in
                          zip(word_numbers, extended_key_numbers)]
 
-    # Преобразование зашифрованных чисел обратно в буквы
+
     encrypted_word = ''.join(number_to_letter[number] for number in encrypted_numbers)
 
     return encrypted_word
@@ -30,18 +29,18 @@ def encrypt(word, key):
 
 
 def decrypt(word, key):
-    # Преобразование слова и ключа в числа
+
     word_numbers = [letter_to_number[letter] for letter in word]
     key_numbers = [letter_to_number[letter] for letter in key]
 
-    # Расшифровка слова
+
     decrypted_numbers = [(word_number - key_number) % 33 for word_number, key_number in
                          zip(word_numbers, key_numbers)]
 
-    # Преобразование расшифрованных чисел обратно в буквы
+
     decrypted_word = ''.join(number_to_letter[number] for number in decrypted_numbers)
 
-    # Расширение ключа до длины слова, если ключ короче
+
     key_numbers = key_numbers + [letter_to_number[letter] for letter in decrypted_word]
     key_numbers = key_numbers[:len(word)]
 
@@ -49,15 +48,15 @@ def decrypt(word, key):
 
 
 
-# Получение слов и ключа от пользователя
+
 word = input("Введите слово: ")
 key = input("Введите ключ: ")
 
-# Проверка длины ключа и слова и соответствующие действия
+
 if len(word) > len(key):
     key = key + word[:len(word) - len(key)]
 
-# Шифрование и расшифровка слова
+
 encrypted_word = encrypt(word, key)
 decrypted_word = decrypt(encrypted_word, key)
 
